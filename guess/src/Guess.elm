745195d10fca58958d2b9ffdcf891135fef5550a
zhenik <nikita.zhevnitskiy@gmail.com>
2019-05-31 20:54:44 +0200

    refactor s.2

--- guess/src/Guess.elm
+++ guess/src/Guess.elm
@@ -1,4 +1,4 @@
-module Guess exposing (..)
+module Guess exposing (Model, Msg(..), checkResult, genResult, initModel, main, revealAndIncrement, update, view)
 
 import Browser exposing (..)
 import Debug exposing (..)
@@ -12,71 +12,66 @@ main =
 
 
 type alias Model =
-    { 
-        word : String, 
-        guess : String, 
-        isCorrect : Bool, 
-        revealedWord: { pos: Int, text: String }
+    { word : String
+    , guess : String
+    , revealedWord : { pos : Int, text : String }
+    , result : String
     }
 
 
 initModel : Model
 initModel =
-    Model "Saturday" "" False {pos= 2, text=""}
+    Model "Saturday" "" { pos = 2, text = "" } ""
 
 
 type Msg
     = Answer String
     | Reveal
+    | Check
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Answer txt ->
-            { model | guess = txt, isCorrect = checkIfCorrect model txt }
+            { model | guess = txt }
 
         Reveal ->
             { model | revealedWord = revealAndIncrement model }
 
-revealAndIncrement : Model -> {pos: Int, text: String}
+        Check ->
+            { model | result = checkResult model }
+
+
+checkResult : Model -> String
+checkResult { word, guess } =
+    if word == guess then
+        "You got it"
+
+    else
+        "No"
+
+
+revealAndIncrement : Model -> { pos : Int, text : String }
 revealAndIncrement { revealedWord, word } =
     if revealedWord.text == word then
-        revealedWord 
-    else
-        { revealedWord | pos = revealedWord.pos + 1, text = String.slice 0 revealedWord.pos word}
-
-
--- revealLetter : Model -> String
--- revealLetter model =
---     if String.length model.word == String.length model.revealedWord then
---         model.word
-
---     else
---         String.slice 0 model.revealedPos model.word
-
-
-checkIfCorrect : Model -> String -> Bool
-checkIfCorrect model txt =
-    if txt == model.word then
-        True
+        revealedWord
 
     else
-        False
+        { revealedWord | pos = revealedWord.pos + 1, text = String.slice 0 revealedWord.pos word }
 
 
-generateResult : Model -> Html Msg
-generateResult { isCorrect, revealedWord, word } =
-    let 
-        txt =
-            if revealedWord.text == word then
-                "You did not guess it"
-            else if isCorrect then
-                "You got it"
-            else
-                "No"
-    in 
-        text txt
+genResult : Model -> Html Msg
+genResult { result } =
+    if String.length result < 1 then
+        div [] []
+
+    else if result == "No" then
+        div [ style "fontColor" "red" ] [ text result ]
+
+    else
+        div [ style "color" "green" ] [ text result ]
+
 
 view : Model -> Html Msg
 view model =
@@ -84,7 +79,8 @@ view model =
         [ h2 [] [ text ("I am thinking of a word that starts with " ++ model.revealedWord.text) ]
         , input [ placeholder "Type your guesss", onInput Answer ] []
         , button [ onClick Reveal ] [ text "Give me a hint" ]
-        , div [] [ generateResult model ]
+        , button [ onClick Check ] [ text "Submit answer" ]
+        , div [] [ text model.result ]
 
         -- , div [] [ text (toString model) ]
         ]
